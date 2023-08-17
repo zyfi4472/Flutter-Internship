95eb9cd19eed1c995c704a6fead944b92fc16cd1
zyfi4472 <zyfi4472@gmail.com>
2023-08-17 21:28:34 +0500

    Listview builder verticle and horizontal

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -7,121 +7,64 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../components/my_text.dart';
 
 class LoginPage extends StatelessWidget {
-  LoginPage({super.key});
-
-  // text editing controllers
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-
-  // sign user in method
-  void _signIn() {}
+  final List<String> imageUrls = [
+    'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-        designSize: const Size(360, 690),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        builder: (context, child) {
-          return Scaffold(
-            body: Stack(
-              children: [
-                Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  color: const Color.fromARGB(
-                      255, 227, 221, 221), // Background color
-                ),
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: Container(
-                    width: 400.w,
-                    height: 190.h,
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(20.r),
-                        bottomRight: Radius.circular(20.r),
-                      ),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        'Rounded Bottom',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.center,
-                  child: Container(
-                    width: 350.w,
-                    height: 500.h,
-                    decoration: const BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.all(Radius.circular(20))),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          MyTextComponent(
-                            text: 'Login',
-                            fontSize: 40.sp,
-                          ),
-
-                          // const Spacer(
-                          //   flex: 1,
-                          // ),
-                          SizedBox(height: 26.h),
-
-                          MyTextField(
-                            controller: emailController,
-                            labelText: 'Email',
-                            hintText: 'Enter your email',
-                            obscureText: true,
-                            prefixIcon: const Icon(Icons.email),
-                          ),
-                          SizedBox(height: 26.h),
-                          MyTextField(
-                            controller: passwordController,
-                            labelText: 'Password',
-                            hintText: 'Enter your password',
-                            obscureText: true,
-                            prefixIcon: const Icon(Icons.lock),
-                          ),
-                          SizedBox(height: 26.h),
-                          MyButton(
-                            signIn: () {},
-                          ),
-                          SizedBox(height: 16.h),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              MyTextComponent(
-                                text: 'Forgot Password?',
-                                fontSize: 14.sp,
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 16.h),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              MyTextComponent(
-                                text: 'Not Registered? Sign Up',
-                                fontSize: 14.sp,
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          );
-        });
+    return Scaffold(
+      appBar: AppBar(title: const Text('Horizontal Scroll Images')),
+      body: Column(children: [
+        SizedBox(
+          height: 120,
+          child: ListView.builder(
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            itemCount: imageUrls.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ClipOval(
+                    child: Image.network(imageUrls[index], fit: BoxFit.cover)),
+              );
+            },
+          ),
+        ),
+        SizedBox(
+          height: 56,
+        ),
+        SizedBox(
+          height: 500,
+          child: ListView.builder(
+            shrinkWrap: true,
+            scrollDirection: Axis.vertical,
+            itemCount: imageUrls.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Image Title ${index + 1}',
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 8),
+                        Image.network(
+                          imageUrls[index],
+                          height: 250,
+                          fit: BoxFit.fitWidth,
+                        ),
+                      ]));
+            },
+          ),
+        ),
+      ]),
+    );
   }
 }
